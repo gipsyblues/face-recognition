301fb113d78e8e5e865b0af6fe137bc2d5a87032
Shang-Hung Tsai <shtsai7@gmail.com>
2017-11-26 09:58:17 -0800

    initialize cpp and cmake files

---
+++ face_recognition/CMakeFiles/face_recognition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_recognition.dir/face_recognition.cpp.o"
+  "face_recognition.pdb"
+  "face_recognition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/face_recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ face_recognition/face_recognition.cpp
@@ -0,0 +1,20 @@
+/**
+ * Computer Vision
+ * Project 2: Face Recognition Using Eigenface Method
+ * Shang-Hung Tsai
+ */
+
+#include<opencv2/core.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/highgui.hpp>
+
+#include<iostream>
+#include<string>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "hello world" << endl;
+}
